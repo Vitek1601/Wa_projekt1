7b3aae97da771d5fe663c7f5e6857ae967ae6c9e
Tomáš Vítek <vitek2@SPSEJECNANET.CZ>
2022-11-25 10:40:40 +0100

    a

---
+++ 11.11/main.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, render_template
+from flask import request, redirect, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.route('/')
+def registrace():
+    return render_template('registrace.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' , port=8080, debug=False)
